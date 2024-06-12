1e42fdd9f60dfd115a0931582a6965dc82a03d5e
unknown <alaminatghoria@gmail.com>
2024-06-12 11:54:05 +0600

    watch count okay

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Swal from "sweetalert2";
 import { TRecipe, TUser } from "../interface/interface";
 import { useNavigate } from "react-router-dom";
