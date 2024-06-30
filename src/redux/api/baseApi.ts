f3019ac55b5fbb255a5b0a73d14c097e6157d426
alaminhossa1n <alaminatghoria@gmail.com>
2024-06-30 22:17:13 +0600

    url update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
-    // https://recipe-sharing-system-server.vercel.app/api
-    // http://localhost:5000/
+    baseUrl: "https://recipe-sharing-system-server.vercel.app/api",
     credentials: "include",
   }),
   tagTypes: ["recipes", "users", "categories"],
